48a54321effafac8174cbdc183f23097ced10241
Juha Järvi <juha.jarvi@karttakeskus.fi>
2014-09-29 13:34:36 +0300

    Automatically add missing field types to database and refactor. Use deferreds in database code to reduce indentation.

--- store.js
+++ store.js
@@ -2,6 +2,15 @@ var fs=require('fs');
 var pg=require('pg.js');
 var Promise = require('es6-promise').Promise;
 
+var Deferred=function() {
+	var self=this;
+
+	this.promise=new Promise(function(resolve,reject) {
+		self.resolve=resolve;
+		self.reject=reject;
+	});
+};
+
 /** Return reference to function fn that when called, makes fn see scope
   * as "this" variable. */
 function bindToScope(scope, fn) {
@@ -15,60 +24,55 @@ var PgDatabase=function() {
 };
 
 PgDatabase.prototype.connect=function(conf) {
-	var self=this;
+	var defer=new Deferred();
 
-	return(new Promise(function(resolve,reject) {
-		self.client=new pg.Client(conf);
-		self.client.connect(function(err) {
-			if(err) {
-				return(reject('Unable to connect to database: '+err));
-			}
+	this.client=new pg.Client(conf);
+	this.client.connect(function(err) {
+		if(err) return(defer.reject('Unable to connect to database: '+err));
+		defer.resolve();
+	});
 
-			resolve();
-		});
-	}));
+	return(defer.promise);
 }
 
 PgDatabase.prototype.close=function(conf) {
-	var self=this;
-
-	return(new Promise(function(resolve,reject) {
-		resolve(self.client.end());
-	}));
+	return(Promise.resolve(this.client.end()));
 }
 
 PgDatabase.prototype.exec=function() {
 	var query=this.client.query.apply(this.client,arguments);
+	var defer=new Deferred();
 
-	return(new Promise(function(resolve,reject) {
-		query.on('error',function(err) {
-			reject(err);
-		});
+	query.on('error',function(err) {
+		defer.reject(err);
+	});
 
-		query.on('end',function(state) {
-			resolve(state);
-		});
-	}));
+	query.on('end',function(state) {
+		defer.resolve(state);
+	});
+
+	return(defer.promise);
 }
 
 PgDatabase.prototype.querySingle=function() {
 	var query=this.client.query.apply(this.client,arguments);
+	var defer=new Deferred();
+	var result;
 
-	return(new Promise(function(resolve,reject) {
-		var result;
+	query.on('row',function(row) {
+		result=row;
+	});
 
-		query.on('row',function(row) {
-			result=row;
-		});
+	query.on('error',function(err) {
+		defer.reject(err);
+	});
 
-		query.on('error',function(err) {
-			reject(err);
-		});
+	query.on('end',function(state) {
+		if(!result) return(defer.reject('Not found'));
+		defer.resolve(result);
+	});
 
-		query.on('end',function(state) {
-			resolve(result);
-		});
-	}));
+	return(defer.promise);
 }
 
 PgDatabase.prototype.begin=function() {
@@ -89,19 +93,19 @@ var SldInserter=function() {
 }
 
 SldInserter.prototype.connect=function(dbPath) {
-	var self=this;
+	var defer=new Deferred();
 
 	this.db=new PgDatabase();
 
-	return(new Promise(function(resolve,reject) {
-		try {
-			var dbJson=fs.readFileSync(dbPath,'utf-8');
-			self.dbConf=JSON.parse(dbJson);
-			resolve();
-		} catch(e) {
-			return(reject('Unable to read database configuration: '+e));
-		}
-	}).then(self.db.connect(this.dbConf)).then(this.db.begin()));
+	try {
+		var dbJson=fs.readFileSync(dbPath,'utf-8');
+		this.dbConf=JSON.parse(dbJson);
+		defer.resolve();
+	} catch(e) {
+		defer.reject('Unable to read database configuration: '+e);
+	}
+
+	return(defer.promise.then(this.db.connect(this.dbConf)).then(this.db.begin()));
 };
 
 SldInserter.prototype.abort=function() {
@@ -119,12 +123,14 @@ SldInserter.prototype.finish=function() {
 };
 
 SldInserter.prototype.readFile=function(path,name) {
-	return(new Promise(function(resolve,reject) {
-		fs.readFile(path,{encoding:'utf-8'},function(err,data) {
-			if(err) return(reject('Unable to read '+name+': '+err));
-			resolve(data);
-		})
-	}));
+	var defer=new Deferred();
+
+	fs.readFile(path,{encoding:'utf-8'},function(err,data) {
+		if(err) defer.reject('Unable to read '+name+': '+err);
+		defer.resolve(data);
+	})
+
+	return(defer.promise);
 };
 
 SldInserter.prototype.insertTemplate=function(templatePath) {
@@ -135,68 +141,100 @@ SldInserter.prototype.insertTemplate=function(templatePath) {
 	}));
 };
 
-SldInserter.prototype.insertConfig=function(configPath,templateId) {
+SldInserter.prototype.insertFeatureType=function(templateId) {
+	return(this.db.querySingle('INSERT INTO sld_featuretype (template_id,name,title,featuretype_name) VALUES ($1,$2,$3,$4) RETURNING id',[templateId,'','','']));
+};
+
+SldInserter.prototype.insertRule=function(featureTypeInserted,fieldList) {
+	var self=this;
+
+	return(featureTypeInserted.then(function(featureTypeId) {
+		var nameList=fieldList[3].split(';');
+		return(self.db.querySingle('INSERT INTO sld_rule (featuretype_id,name,title,abstract) VALUES ($1,$2,$3,$4) RETURNING id',[featureTypeId.id,nameList[0],nameList[1],nameList[2]]));
+	}));
+};
+
+SldInserter.prototype.insertParam=function(ruleInserted,fieldList) {
+	var self=this;
+	var defer=new Deferred();
+
+	ruleInserted.then(function(ruleId) {
+		var typeFound=self.db.querySingle('SELECT id,name,symbolizer FROM sld_type WHERE symbolizer=$1',[fieldList[4]]);
+
+		typeFound.then(function(typeRow) {
+			self.db.querySingle('INSERT INTO sld_param (rule_id,template_offset,type_id,default_value) VALUES ($1,$2,$3,$4) RETURNING id',[ruleId.id,fieldList[3],typeRow.id,fieldList[5]]).then(function() {
+				defer.resolve();
+			});
+		});
+
+		typeFound.catch(function() {
+			return(self.db.querySingle('INSERT INTO sld_type (name,symbolizer) VALUES ($1,$2) RETURNING id',['',fieldList[4]]));
+		}).then(function(typeRow) {
+			self.db.querySingle('INSERT INTO sld_param (rule_id,template_offset,type_id,default_value) VALUES ($1,$2,$3,$4) RETURNING id',[ruleId.id,fieldList[3],typeRow.id,fieldList[5]]).then(function() {
+				defer.resolve();
+			});
+		}).catch(function(err) {defer.reject(err);});
+	});
+
+	return(defer.promise);
+};
+
+SldInserter.prototype.insertConfig=function(configPath,templateRow) {
 	var promiseList=[];
-	var readyCount=0;
 
 	var self=this;
 
-	return(this.readFile(configPath,'SLD config').then(function(sldConfig) {
-		return(new Promise(function(resolve,reject) {
-			var lineList;
-			var lineNum,lineCount;
-			var fieldList;
-			var featureTypeInserted,ruleInserted,fieldInserted;
+	var inputReady=this.readFile(configPath,'SLD config');
+	var ready=inputReady.then(function(sldConfig) {
+		var defer=new Deferred();
+		var lineList;
+		var lineNum,lineCount;
+		var fieldList;
+		var featureTypeInserted,ruleInserted,fieldInserted;
 
-			lineList=sldConfig.split(/\r?\n/);
-			lineCount=sldConfig.length;
+		lineList=sldConfig.split(/\r?\n/);
+		lineCount=sldConfig.length;
 
-			for(lineNum=0;lineNum<lineCount;lineNum++) {
-				line=lineList[lineNum];
-				if(!line) continue;
-				fieldList=line.split('\t');
+		for(lineNum=0;lineNum<lineCount;lineNum++) {
+			line=lineList[lineNum];
+			if(!line) continue;
+			fieldList=line.split('\t');
 
-				type=fieldList[0];
+			type=fieldList[0];
 
-				if(type=='FeatureType') {
-					featureTypeInserted=self.db.querySingle('INSERT INTO sld_featuretype (template_id,name,title,featuretype_name) VALUES ($1,$2,$3,$4) RETURNING id',[templateId.id,'','','']);
-					featureTypeInserted.catch(function(err) {
-						reject('Error inserting feature type: '+err);
-					});
-					promiseList.push(featureTypeInserted);
-				}
-
-				if(type=='Rule') {
-					ruleInserted=(function(fieldList) {
-						return(featureTypeInserted.then(function(featureTypeId) {
-							var nameList=fieldList[3].split(';');
-							return(self.db.querySingle('INSERT INTO sld_rule (featuretype_id,name,title,abstract) VALUES ($1,$2,$3,$4) RETURNING id',[featureTypeId.id,nameList[0],nameList[1],nameList[2]]));
-						}));
-					})(fieldList);
-					ruleInserted.catch(function(err) {
-						reject('Error inserting rule: '+err);
-					});
-					promiseList.push(ruleInserted);
-				}
-
-				if(type=='Field') {
-					fieldInserted=(function(fieldList) {
-						return(ruleInserted.then(function(ruleId) {
-							return(self.db.querySingle('INSERT INTO sld_param (rule_id,template_offset,type_id,default_value) VALUES ($1,$2,$3,$4) RETURNING id',[ruleId.id,fieldList[3],1,fieldList[5]]));
-						}));
-					})(fieldList);
-					fieldInserted.catch(function(err) {
-						reject('Error inserting field: '+err);
-					});
-					promiseList.push(fieldInserted);
-				}
+			if(type=='FeatureType') {
+				featureTypeInserted=self.insertFeatureType(templateRow.id);
+				featureTypeInserted.catch(function(err) {
+					defer.reject('Error inserting feature type: '+err);
+				});
+				promiseList.push(featureTypeInserted);
 			}
 
-			Promise.all(promiseList).then(function() {
-				resolve();
-			});
-		}));
-	}));
+			if(type=='Rule') {
+				ruleInserted=self.insertRule(featureTypeInserted,fieldList);
+				ruleInserted.catch(function(err) {
+					defer.reject('Error inserting rule: '+err);
+				});
+				promiseList.push(ruleInserted);
+			}
+
+			if(type=='Field') {
+				paramInserted=self.insertParam(ruleInserted,fieldList);
+				paramInserted.catch(function(err) {
+					defer.reject('Error inserting field: '+err);
+				});
+				promiseList.push(paramInserted);
+			}
+		}
+
+		Promise.all(promiseList).then(function() {
+			defer.resolve();
+		});
+
+		return(defer.promise);
+	});
+
+	return(ready);
 };
 
 function run() {
@@ -208,8 +246,8 @@ function run() {
 		return(inserter.insertTemplate(process.argv[2]));
 	});
 
-	var configInserted=templateInserted.then(function(templateId) {
-		return(inserter.insertConfig(process.argv[3],templateId));
+	var configInserted=templateInserted.then(function(templateRow) {
+		return(inserter.insertConfig(process.argv[3],templateRow));
 	});
 
 	var ready=configInserted;
