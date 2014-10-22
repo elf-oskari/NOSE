6b23a08bddded8c71391752d02368cebae4df261
Mikael Blomberg <mikael.blomberg@nls.fi>
2014-10-22 08:41:20 +0300

    Changed Leaflet to OpenLayers3 Removed all references to Leaflet and replaced with ol3 Modified SLDmap to use ol3 instead of Leaflet Shimmed ol3 in require to obtain ol reference, which unfortunately remains global

--- public/javascripts/views/SLDmap.js
+++ public/javascripts/views/SLDmap.js
@@ -2,9 +2,9 @@ define([
   'lodash',
   'backbone',
   'jquery',
-  'leaflet',
+  'ol3',
   'i18n!localization/nls/SLDlist'
-], function(_, Backbone, $, Leaflet, locale) {
+], function(_, Backbone, $, ol, locale) {
   var SLDMapView = Backbone.View.extend({
     className: 'map',
     initialize: function() {
@@ -13,18 +13,25 @@ define([
     render: function() {
       // create a map in the "map" div, set the view to a given place and zoom
       if (!this.map) {
-        this.map = Leaflet.map(this.el);
-        // add an OpenStreetMap tile layer
-        Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.map);
+        this.map = new ol.Map({
+          target: this.el,
+          layers: [
+            new ol.layer.Tile({source: new ol.source.OSM()})
+          ],
+          view: new ol.View({
+            center: [-17704.73, 6710951.33],
+            zoom: 13
+          })
+        });
+        window.debugMap = this.map;
       } else {
         // map node has been detached.
         // Note! event handling might not function properly, but since we currently do not have any map specific
         // event handling, this is not tested. Look at assign in SLDEditorPage for more details.
-        this.$el.replaceWith(this.map.getContainer());
+        this.$el.replaceWith(this.map.getTarget());
+        // reset map view
+        this.map.getView().setCenter([-17704.73, 6710951.33], 13);
       }
-      this.map.setView([51.505, -0.09], 13);
     }
   });
   return SLDMapView;
