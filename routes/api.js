e7f87ea4f58df75f3f5c6056bca933a5c607dede
Hanna Visuri <hanna.visuri@nls.fi>
2015-02-17 10:10:17 +0200

    Trying to fix reverse proxy

--- routes/api.js
+++ routes/api.js
@@ -113,9 +113,10 @@ module.exports = function (app, path, client, data, libs) {
     }
 
     function setResLocation (path, res) {
-        var absolutePath = (data.baseUrl || '') + path;
-        console.log('Redirecting to ' + absolutePath);
-        res.redirect(absolutePath);
+        //var absolutePath = (data.baseUrl || '') + path;
+        //console.log('Redirecting to ' + absolutePath);
+        res.location(path);
+        res.redirect(path);
     }
 
     app.post('/login', passport.authenticate('local', { failureRedirect:(data.baseUrl || '') +'/' }), function(req, res, next) {
